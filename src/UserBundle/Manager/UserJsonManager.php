9e5758aa9564bd1a726f03ad373c74e82d8d06e0
Goose <goose@zeoalliance.com>
2019-01-30 14:31:58 +0200

    manager change

--- src/UserBundle/Manager/UserJsonManager.php
+++ src/UserBundle/Manager/UserJsonManager.php
@@ -7,10 +7,17 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class UserJsonManager implements EntityManagerInterface
 {
+    private $usersDir;
+
+    public function __construct(string $usersDir)
+    {
+        $this->usersDir = $usersDir;
+    }
+
     public function getRepository($className)
     {
         $fileSystem = new Filesystem();
-        return new UserRepository($this, $className, $fileSystem);
+        return new UserRepository($this, $className, $fileSystem, $this->usersDir);
     }
 
     public function find($className, $id)
